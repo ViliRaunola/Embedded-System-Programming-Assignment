37150801d6d2cf6e18ff8843352b9d06f5bd70cc
Vili Raunola <raunolavili@gmail.com>
2023-11-09 16:41:10 +0200

    FreeRTOS with example program

---
+++ assignment/Debug/src/rtos_example.d
@@ -0,0 +1,61 @@
+src/rtos_example.o src/rtos_example.o: ../src/rtos_example.c \
+ ../../assignment_bsp/ps7_cortexa9_0/include/FreeRTOS.h \
+ ../../assignment_bsp/ps7_cortexa9_0/include/FreeRTOSConfig.h \
+ ../../assignment_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../assignment_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../assignment_bsp/ps7_cortexa9_0/include/projdefs.h \
+ ../../assignment_bsp/ps7_cortexa9_0/include/portable.h \
+ ../../assignment_bsp/ps7_cortexa9_0/include/deprecated_definitions.h \
+ ../../assignment_bsp/ps7_cortexa9_0/include/portmacro.h \
+ ../../assignment_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../assignment_bsp/ps7_cortexa9_0/include/mpu_wrappers.h \
+ ../../assignment_bsp/ps7_cortexa9_0/include/task.h \
+ ../../assignment_bsp/ps7_cortexa9_0/include/list.h \
+ ../../assignment_bsp/ps7_cortexa9_0/include/semphr.h \
+ ../../assignment_bsp/ps7_cortexa9_0/include/queue.h \
+ ../../assignment_bsp/ps7_cortexa9_0/include/task.h \
+ ../../assignment_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../assignment_bsp/ps7_cortexa9_0/include/bspconfig.h \
+ ../../assignment_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../assignment_bsp/ps7_cortexa9_0/include/xil_types.h \
+ G:\Opiskelu\Yliopisto\5.vuosi\EmbeddedSystemsProgramming\assignment\zybo_platform/zynq_registers.h
+
+../../assignment_bsp/ps7_cortexa9_0/include/FreeRTOS.h:
+
+../../assignment_bsp/ps7_cortexa9_0/include/FreeRTOSConfig.h:
+
+../../assignment_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../assignment_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../assignment_bsp/ps7_cortexa9_0/include/projdefs.h:
+
+../../assignment_bsp/ps7_cortexa9_0/include/portable.h:
+
+../../assignment_bsp/ps7_cortexa9_0/include/deprecated_definitions.h:
+
+../../assignment_bsp/ps7_cortexa9_0/include/portmacro.h:
+
+../../assignment_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../assignment_bsp/ps7_cortexa9_0/include/mpu_wrappers.h:
+
+../../assignment_bsp/ps7_cortexa9_0/include/task.h:
+
+../../assignment_bsp/ps7_cortexa9_0/include/list.h:
+
+../../assignment_bsp/ps7_cortexa9_0/include/semphr.h:
+
+../../assignment_bsp/ps7_cortexa9_0/include/queue.h:
+
+../../assignment_bsp/ps7_cortexa9_0/include/task.h:
+
+../../assignment_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../assignment_bsp/ps7_cortexa9_0/include/bspconfig.h:
+
+../../assignment_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../assignment_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+G:\Opiskelu\Yliopisto\5.vuosi\EmbeddedSystemsProgramming\assignment\zybo_platform/zynq_registers.h:
